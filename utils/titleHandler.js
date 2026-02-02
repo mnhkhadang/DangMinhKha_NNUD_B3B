c7231ae8da1df23f21796150e4b519de72dfab32
mnhkhadang <159155420+mnhkhadang@users.noreply.github.com>
2026-02-02 11:09:41 +0700

    hoan thanh

---
+++ utils/titleHandler.js
@@ -0,0 +1,14 @@
+module.exports = {
+  ConvertTitleToSlug: function (title) {
+    let result = title
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .replace(/đ/g, "d")
+      .replace(/[^a-z0-9\s-]/g, "")
+      .trim()
+      .replace(/\s+/g, "-");
+
+    return result;
+  }
+};
